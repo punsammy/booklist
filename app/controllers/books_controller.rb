79480ee4e63eb5f0b92adf30352ea8c71b20e025
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-26 15:24:09 -0400

    add CSV response

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,24 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    respond_to :html, :text
+    # respond_to :html, :text  one way of doing it
+    respond_to do |format|
+      format.html
+      format.text
+      format.csv do
+        def self.generate_csv(book_list)
+          header = ['id', 'title', 'author', 'already_read']
+
+          CSV.generate(headers: true) do |csv|
+            csv << header
+
+            book_list.each do |book|
+              csv << [book.id, book.title, book.author, book.already_read]
+            end
+          end
+        end
+      end
+    end
 
   end
 
